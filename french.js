69bbd46fd298d11c2af76d797347bd7313c3de1a
ZKillou <55600416+ZKillou@users.noreply.github.com>
2020-07-10 21:25:32 +0200

    french.js init

---
+++ french.js
@@ -0,0 +1,7 @@
+module.exports = {
+ HELP: {
+  CMD_LIST: "<:Help:728617506964635659> | Liste des commandes • {{nb}} commandes",
+  CMD_RAPPEL: "<a:notif:719806925788676157> | **Rappel :** Les arguments entre `<>` sont obligatoires, les arguments entre `[]` sont facultatifs.",
+ }
+ 
+}

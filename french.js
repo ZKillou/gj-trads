6c646ce33df6acf25cf82b9cab4caef1190e3bed
ZKillou <55600416+ZKillou@users.noreply.github.com>
2020-07-10 21:28:39 +0200

    Update french.js

--- french.js
+++ french.js
@@ -2,6 +2,8 @@ module.exports = {
  HELP: {
   CMD_LIST: "<:Help:728617506964635659> | Liste des commandes • {{nb}} commandes",
   CMD_RAPPEL: "<a:notif:719806925788676157> | **Rappel :** Les arguments entre `<>` sont obligatoires, les arguments entre `[]` sont facultatifs.",
+ },
+ PING: {
+  API: "",
  }
- 
 }

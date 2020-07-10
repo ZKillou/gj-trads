2efc3189fb35dcd0250a90b6f82442a6b7b06f71
ZKillou <55600416+ZKillou@users.noreply.github.com>
2020-07-10 21:35:24 +0200

    Ping + not translate

--- french.js
+++ french.js
@@ -1,9 +1,13 @@
 module.exports = {
+ NOT_TRANSLATE: "Désolé, ceci n'est pas encore traduit en **FRANÇAIS**.",
  HELP: {
   CMD_LIST: "<:Help:728617506964635659> | Liste des commandes • {{nb}} commandes",
   CMD_RAPPEL: "<a:notif:719806925788676157> | **Rappel :** Les arguments entre `<>` sont obligatoires, les arguments entre `[]` sont facultatifs.",
  },
  PING: {
-  API: "",
+  API: "<a:DiscordFr_SpinDiscord:724945863214104577> | Latence de l'API :",
+  ROBOT: "<:DiscordFr_Bot:724946149299191818> | Latence du robot : ",
+  MSG: "<a:DiscordFr_Typing:724946684563554385> | Latence du message :",
+  QUALITE: "<a:DiscordFr_BlueLoading:724947426477080647> | Qualité de la latence du robot :"
  }
 }

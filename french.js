48117ece1a423d12bce98fec06deff482acc0efa
ZKillou <55600416+ZKillou@users.noreply.github.com>
2020-07-11 14:33:49 +0200

    CRIME

--- french.js
+++ french.js
@@ -29,5 +29,11 @@ module.exports = {
   },
   BUY: (item) => let answer = `Vous avez acheté **${name}** avec succès !`
  },
- WITHDRAW: (amt) => let answer = `Vous avez retiré **${amt} <:NewGoldus:725023149741047829>** de votre compte en banque avec succès !`
+ WITHDRAW: (amt) => let answer = `Vous avez retiré **${amt} <:NewGoldus:725023149741047829>** de votre compte en banque avec succès !`,
+ CRIME: {
+  SUCCES: (amt) => let answer = `**:gem: Vous venez de dévaliser une banque, vous obtenez ${amt} <:NewGoldus:725023149741047829>**`,
+  SUCCES_AUTHOR: "Butin de"
+  FAILED: (amt) => let answer = `**:police_officer: Vous sortez d'un cambriolage mais la police vous retrouve, vous devez payer ${amt} <:NewGoldus:725023149741047829>**`,
+  FAILED_AUTHOR: "Amende de"
+ }
 }

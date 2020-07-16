72085135db027953fc918edfe48613afdc65f93f
ZKillou <55600416+ZKillou@users.noreply.github.com>
2020-07-16 15:20:47 +0200

    MOD added in english

--- english.js
+++ english.js
@@ -84,5 +84,65 @@ module.exports = {
    WATCHING: "Watching :",
    STREAM: "Streaming :"
   }
+ },
+ MOD: {
+  NO_REASON: "No reason specified",
+  BAN: {
+   BANNED: (user, reason) => `**<:CheckYes:719807502966849627> You successfully banished ${user} with reason: \`${reason}\`!**`,
+   BANNED_DM: (guildname, reason) => `**<:Warning:719808381593976842> You have been banned from the guild \`${guildname}\` with reason: \`${reason}\`!**`,
+   AUDIT_LOGS: (mod, reason) => `Mod: ${mod} | Reason: ${reason}`,
+   I_CANT: "**<:CheckNo:719807585015824414> I can't ban this user!**",
+   YOU_CANT: "**<:CheckNo:719807585015824414> You can't ban this user!**",
+   ALDEADY_BANNED: (user) => `**<:CheckNo:719807585015824414> The user ${user} has already been banned from the guild!**`
+  },
+  UNBAN: {
+   UNBANNED: (user, reason) => `**<:CheckYes:719807502966849627> You successfully unban ${user} with reason: \`${reason}\`!**`,
+   UNBANNED_DM: (guildname, reason) => `**<:Warning:719808381593976842> You have been unbanned from the guild \`${guildname}\` with reason: \`${reason}\`!**`,
+   AUDIT_LOGS: (mod, reason) => `Mod: ${mod} | Reason: ${reason}`,
+   ALDEADY_UNBANNED: (user) => `**<:CheckNo:719807585015824414> The user ${user} has already been unbanned from the guild!**`
+  },
+  KICK: {
+   KICKED: (user, reason) => `**<:CheckYes:719807502966849627> You successfully kicked ${user} with reason: \`${reason}\`!**`,
+   KICKED_DM: (guildname, reason) => `**<:Warning:719808381593976842> You have been kicked from the guild \`${guildname}\` with reason: \`${reason}\`!**`,
+   AUDIT_LOGS: (mod, reason) => `Mod: ${mod} | Reason: ${reason}`,
+   I_CANT: "**<:CheckNo:719807585015824414> I can't kick this user!**",
+   YOU_CANT: "**<:CheckNo:719807585015824414> You can't kick this user!**"
+  },
+  MUTE: {
+   MUTED: (user, reason) => `**<:CheckYes:719807502966849627> You successfully muted ${user} with reason: \`${reason}\`!**`,
+   MUTED_DM: (guildname, reason) => `**<:Warning:719808381593976842> You have been muted from the guild \`${guildname}\` with reason: \`${reason}\`!**`,
+   AUDIT_LOGS: (mod, reason) => `Mod: ${mod} | Reason: ${reason}`,
+   I_CANT: "**<:CheckNo:719807585015824414> I can't mute this user!**",
+   YOU_CANT: "**<:CheckNo:719807585015824414> You can't mute this user!**",
+   ALDEADY_MUTED: (user) => `**<:CheckNo:719807585015824414> The user ${user} has already been muted from the guild!**`
+  },
+  UNMUTE: {
+   UNMUTED: (user, reason) => `**<:CheckYes:719807502966849627> You successfully unmuted ${user} with reason: \`${reason}\`!**`,
+   UNMUTED_DM: (guildname, reason) => `**<:Warning:719808381593976842> You have been unmuted from the guild \`${guildname}\` with reason: \`${reason}\`!**`,
+   AUDIT_LOGS: (mod, reason) => `Mod: ${mod} | Reason: ${reason}`,
+   I_CANT: "**<:CheckNo:719807585015824414> I can't unmute this user!**",
+   YOU_CANT: "**<:CheckNo:719807585015824414> You can't unmute this user!**",
+   ALDEADY_UNMUTED: (user) => `**<:CheckNo:719807585015824414> The user ${user} has already been unmuted from the guild!**`
+  },
+  WARN: {
+   WARNED: (user, reason) => `**<:CheckYes:719807502966849627> You successfully gave a warning to ${user} with reason: \`${reason}\`!**`,
+   WARNED_DM: (guildname, reason) => `**<:Warning:719808381593976842> You have received a warning from the guild \`${guildname}\` for the reason: \`${reason}\`!**`,
+   I_CANT: "**<:CheckNo:719807585015824414> I can't warn this user!**",
+   YOU_CANT: "**<:CheckNo:719807585015824414> You can't warn this user!**"
+  },
+  UNWARN: {
+   UNWARNED: (user, reason) => `**<:CheckYes:719807502966849627> You successfully removed a warning to ${user} with reason: \`${reason}\`!**`,
+   UNWARNED_DM: (guildname, reason) => `**<:Warning:719808381593976842> A warning has been removed from the guild \`${guildname}\` for the reason: \`${reason}\`!**`,
+   YOU_CANT: "**<:CheckNo:719807585015824414> You can't unwarn this user!**"
+  },
+  CLEARWARN: {
+   CLEARWARNED: (user, reason) => `**<:CheckYes:719807502966849627> You successfully deleted all ${user}'s warnings with reason: \`${reason}\`!**`,
+   CLEARWARNED_DM: (guildname, reason) => `**<:Warning:719808381593976842> All of your warnings has been removed from the guild \`${guildname}\` for the reason: \`${reason}\`!**`,
+   YOU_CANT: "**<:CheckNo:719807585015824414> You can't clear all warnings of this user!**"
+  },
+  CLEAR: {
+   CLEARED: (amt) => `**<:CheckYes:719807502966849627> You successfully deleted \`${amt}\` messages!**`,
+   AUDIT_LOGS: (mod, reason) => `Mod: ${mod}`
+  },
  }
 }
